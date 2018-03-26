8a85f62406d440d1811194eb798ebb91f651207a
Wim Looman <wim.looman@kiwi.ki>
2018-03-26 17:55:21 +0200

    Add generated docs

---
+++ target/doc/implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["inner"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"inner/struct.Timer.html\" title=\"struct inner::Timer\">Timer</a>",synthetic:true,types:["inner::Timer"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = {};
+searchIndex["inner"] = {"doc":"","items":[[3,"Timer","inner","",null,null],[11,"timeout","","",0,null]],"paths":[[3,"Timer"]]};
+searchIndex["temp"] = {"doc":"","items":[[3,"Timer","temp","",null,null],[11,"timeout","","",0,null]],"paths":[[3,"Timer"]]};
+initSearch(searchIndex);
